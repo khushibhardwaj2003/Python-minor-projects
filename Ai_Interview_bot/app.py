bffcdbfbbfda166449c502df9c71122f6364f951
khushibhardwaj2003 <khushi1.2026@iic.ac.in>
2026-08-31 12:49:47 +0530

    changes into css

--- Ai_Interview_bot/app.py
+++ Ai_Interview_bot/app.py
@@ -3,47 +3,40 @@ from ollama import Client
 
 app = Flask(__name__)
 
-client = Client(host="http://localhost:11434")
-
+client = Client(host="d75e1c954fda4258afa83154520f91d3.GUVWnfun0tztuJ4r0_ojXwwP")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/generate", methods=["POST"])
 def generate():
- 
+
     data = request.json
 
     subject = data["subject"]
-    num_questions = int(data["num_questions"])
+    num_questions = data["num_questions"]
 
     prompt = f"""
     Generate EXACTLY {num_questions} interview questions
-    with answers on the topic "{subject}".
+    with answers on {subject}.
 
-    Rules:
-    - Generate exactly {num_questions} questions.
-    - Number them Q1, Q2, Q3...
-    - Give a short answer after each question.
-    - Do not stop before generating all questions.
+    Format:
+
+    Q.1 Question
+    Ans. Answer
     """
 
     response = client.chat(
-        model="phi3:mini",
+        model="phi3:mini",   # or llama3.2
         messages=[
             {"role": "user", "content": prompt}
-        ],
-        options={
-            "num_predict": 2500
-        }
+        ]
     )
 
     return jsonify({
         "response": response["message"]["content"]
     })
-
 
 if __name__ == "__main__":
     app.run(debug=True)
